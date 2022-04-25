24f010afa237c97a4065bc9170b280839eaa27ac
Toan Nguyen <nntoan@users.noreply.github.com>
2022-04-26 00:00:28 +0700

    Support Magepack bundling

---
+++ src/Task/MagepackBundleTask.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * FrontoolsSetup.
+ *
+ * @category  MageMagento
+ *
+ * @author    Toan Nguyen <nntoan@users.noreply.github.com>
+ */
+
+namespace Mage\Magento\Task;
+
+/**
+ * Generate Magepack bundle.
+ *
+ * on-deploy:
+ *     - magepack/bundle { timeout: 300,  }
+ */
+class MagepackBundleTask extends AbstractTask
+{
+    public function getName()
+    {
+        return 'magepack/bundle';
+    }
+
+    public function getDescription()
+    {
+        return '[Magepack] Generating Bundle';
+    }
+
+    public function execute()
+    {
+        $timeout = 300;
+        $path = 'magepack.config.js';
+
+        if (array_key_exists('timeout', $this->options)) {
+            $timeout = $this->options['timeout'];
+        }
+
+        if (array_key_exists('path', $this->options)) {
+            $path = $this->options['path'];
+        }
+
+        $cmd = $this->buildCustomCommand('src/tools', sprintf('yarn magepackBundle --config %s', $path));
+
+        $process = $this->runtime->runCommand(trim($cmd), $timeout);
+
+        return $process->isSuccessful();
+    }
+}
